e7b8d55a92de65598818a936fdb85ec09a70f859
Baekshook <ecgo0106@naver.com>
2023-04-10 14:54:46 +0900

    서버와 통신 작업

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,15 +1,44 @@
-import React from "react";
+import axios from "axios";
+import { useState } from "react";
 
-export default function Chat() {
-  const onSubmitChat = (e) => {
-    e.preventDefault();
+const Chat = () => {
+  const [question, setQuestion] = useState("");
+
+  const onSubmitChat = async (e) => {
+    try {
+      e.preventDefault();
+
+      const response = await axios.post(
+        "https://holy-fire-2749.fly.dev/chat",
+        {
+          question: "안녕?",
+        },
+        {
+          headers: {
+            Authorization: "Bearer BLOCKCHAINSCHOOL3",
+          },
+        }
+      );
+
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
   };
+
   return (
-    <div className=" bg-black min-h-screen flex justify-center items-center text-white">
+    <div className="bg-black min-h-screen flex justify-center items-center text-white">
       <form onSubmit={onSubmitChat}>
-        <input className="text-black" type="text" />
-        <input className="ml-3" type="submit" value="검색" />
+        <input
+          className="text-black"
+          type="text"
+          value={question}
+          onChange={(e) => setQuestion(e.target.value)}
+        />
+        <input className="ml-3" type="submit" value="검 색" />
       </form>
     </div>
   );
-}
+};
+
+export default Chat;
